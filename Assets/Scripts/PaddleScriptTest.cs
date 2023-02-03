50b5d56008e5a591e66e4420577bbed159549a51
Olatiny <70335512+Olatiny@users.noreply.github.com>
2023-02-03 03:09:33 -0500

    android build settings & made fullscreen work in webgl

--- Assets/Scripts/PaddleScriptTest.cs
+++ Assets/Scripts/PaddleScriptTest.cs
@@ -19,6 +19,12 @@ public class PaddleScriptTest : MonoBehaviour
     [SerializeField]
     private float damperStrength = 30f;
 
+    [SerializeField]
+    private float min_x_touch;
+
+    [SerializeField]
+    private float max_x_touch;
+
     private HingeJoint2D hinge;
 
     bool playSound = true;
@@ -43,7 +49,60 @@ public class PaddleScriptTest : MonoBehaviour
         if (GameManager.Instance.state == GameManager.GameState.GameOver || GameManager.Instance.state == GameManager.GameState.Paused)
             return;
 
-        if (Input.GetKey(key) || Input.GetKey(altKey))
+        if (Input.touchCount > 0)
+        {
+            Touch touch1 = Input.GetTouch(0);
+
+            if (touch1.phase == TouchPhase.Began || touch1.phase == TouchPhase.Stationary || touch1.phase == TouchPhase.Moved)
+            {
+                Vector2 pos1 = Camera.main.ScreenToWorldPoint(touch1.position);
+
+                if (pos1.x > min_x_touch && pos1.x < max_x_touch)
+                {
+                    if (playSound)
+                    {
+                        playSound = false;
+                        GetComponent<AudioSource>().Play();
+                    }
+                    //SoundManager.instance.PlaySoundEffect(SoundManager.instance.paddleSwing);
+                    GetComponent<Rigidbody2D>().AddTorque(hitStrength);
+                }
+            }
+
+
+            if (Input.touchCount >= 2)
+            {
+                Touch touch2 = Input.GetTouch(1);
+
+                if (touch2.phase == TouchPhase.Began || touch2.phase == TouchPhase.Stationary || touch2.phase == TouchPhase.Moved || touch1.phase == TouchPhase.Began || touch1.phase == TouchPhase.Stationary || touch1.phase == TouchPhase.Moved)
+                {
+                    Vector2 pos1 = Camera.main.ScreenToWorldPoint(touch1.position);
+                    Vector2 pos2 = Camera.main.ScreenToWorldPoint(touch2.position);
+
+                    if (pos1.x > min_x_touch && pos1.x < max_x_touch)
+                    {
+                        if (playSound)
+                        {
+                            playSound = false;
+                            GetComponent<AudioSource>().Play();
+                        }
+                        //SoundManager.instance.PlaySoundEffect(SoundManager.instance.paddleSwing);
+                        GetComponent<Rigidbody2D>().AddTorque(hitStrength);
+                    }
+                    else if (pos2.x > min_x_touch && pos2.x < max_x_touch)
+                    {
+                        if (playSound)
+                        {
+                            playSound = false;
+                            GetComponent<AudioSource>().Play();
+                        }
+                        //SoundManager.instance.PlaySoundEffect(SoundManager.instance.paddleSwing);
+                        GetComponent<Rigidbody2D>().AddTorque(hitStrength);
+                    }
+                }
+            }
+        } 
+        else if (Input.GetKey(key) || Input.GetKey(altKey))
         {
             if (playSound)
             {
